d8a009b361457bb38d3c04c2c36cf568fe66c168
maryam-amn <meryem.2005.aman@gmail.com>
2026-06-26 10:18:46 +0200

    feat: view a playable character on the api

--- test/controllers/api/v1/playable_characters_controller_test.rb
+++ test/controllers/api/v1/playable_characters_controller_test.rb
@@ -15,4 +15,24 @@ class Api::V1::PlayableCharactersControllerTest < ActionDispatch::IntegrationTes
     assert_includes response.parsed_body["playable_character"], playable_characters_to_json.as_json
     assert_equal PlayableCharacter.count,  response.parsed_body["playable_character"].count
   end
+  test "Should view a playable character if we find their ID" do
+    playable_character = playable_characters(:charlotte_from_fontaine_region)
+    get api_v1_playable_character_url(id: playable_character.id)
+
+    assert_response :success
+
+    playable_character_to_json = PlayableCharacterJson.new(playable_character:).to_h
+
+    assert_equal response.parsed_body, playable_character_to_json.as_json
+  end
+
+  test "Shouldn't be able to see a playable character if we can't find their ID and should return an error" do
+    get api_v1_playable_character_url(id: 0)
+
+    assert_response :not_found
+
+    error_message = I18n.t("Playable_Characters.errors.record_not_found")
+
+    assert_includes response.parsed_body[:error], error_message.as_json
+  end
 end
