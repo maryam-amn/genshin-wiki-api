eff3c52181a91b17f580fffe3f13de38dd0d1be0
maryam-amn <meryem.2005.aman@gmail.com>
2026-06-29 14:57:20 +0200

    feat: add the create action to be able to create a playable character on the api clsoses #65

--- test/controllers/api/v1/playable_characters_controller_test.rb
+++ test/controllers/api/v1/playable_characters_controller_test.rb
@@ -35,4 +35,62 @@ class Api::V1::PlayableCharactersControllerTest < ActionDispatch::IntegrationTes
 
     assert_includes response.parsed_body[:error], error_message.as_json
   end
+
+  test "Should be able to create a playable character on the api " do
+    post api_v1_playable_characters_path, params: {  "name": "Venti",
+                                                       "description": "Venti est un barde à l'esprit libre, un amateur du vin de Mondstadt et la forme mortelle actuelle de Barbatos, l'Archon Anémo",
+                                                       "rarity": 5,
+                                                       "region": "Montstadt",
+                                                       "base_hp": 819.86,
+                                                       "base_defense": 52.05,
+                                                       "base_attack": 20.48,
+                                                       "is_limited": true
+                                                    }
+
+    assert_response :success
+
+    playable_character = PlayableCharacter.last
+
+    playable_character_to_json = PlayableCharacterJson.new(playable_character:).to_h
+
+    assert_equal response.parsed_body, playable_character_to_json.as_json
+  end
+
+  test "Shouldn't be able to create a playable character if a required field is missing " do
+    post api_v1_playable_characters_path, params: {  "name": "",
+                                                     "description": "Charlotte est un personnage Cryo 4 étoile, journaliste pour L'Oiseau de vapeur, le célébre journal de Fontaine",
+                                                     "rarity": 3,
+                                                     "region": "Fontaine",
+                                                     "base_hp": "",
+                                                     "base_defense": 45.78,
+                                                     "base_attack": 14.51,
+                                                     "is_limited": true
+    }
+    assert_response :unprocessable_entity
+
+    error_message = {
+      error: I18n.t("Playable_Characters.create.record_invalid"), details: { field: [
+          "Validation failed: Base hp can't be blank" ] } }
+
+    assert_equal response.parsed_body, error_message.as_json
+  end
+
+  test "Shouldn't create a playable character with the same name as another one" do
+    post api_v1_playable_characters_path, params: { "name": "Charlotte",
+                                                     "description": "Charlotte est un personnage Cryo 4 étoile, journaliste pour L'Oiseau de vapeur, le célébre journal de Fontaine",
+                                                     "rarity": 3,
+                                                     "region": "Fontaine",
+                                                     "base_hp": 50,
+                                                     "base_defense": 45.78,
+                                                     "base_attack": 14.51,
+                                                     "is_limited": true
+    }
+    assert_response :unprocessable_entity
+
+    error_message = {
+      error: I18n.t("Playable_Characters.create.record_invalid"), details: { field: [
+        "Validation failed: Name has already been taken" ] } }
+
+    assert_equal response.parsed_body, error_message.as_json
+  end
 end
